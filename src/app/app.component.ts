94f6577eccd0d7ea07eb5f98800da21ce892c0d9
littlehorseboy <littlehorseboy@gmail.com>
2020-03-25 13:08:51 +0800

    feat: pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const observable = new Observable<number>((observer) => {
   observer.next(1);
@@ -7,9 +8,18 @@ const observable = new Observable<number>((observer) => {
 
 let a = 1;
 
-observable.subscribe((value) => {
-  a = value;
-});
+// observable.subscribe((value) => {
+//   a = value;
+// });
+
+observable
+  .pipe(
+    map((value) => value * 2),
+  )
+  .subscribe((value) => {
+    a = value;
+  });
+
 
 @Component({
   selector: 'app-root',
